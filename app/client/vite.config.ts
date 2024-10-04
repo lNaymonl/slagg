d76222b78ec992619fa36db02f81258d2a25391b
Elmo Heine <elmo.heine@web.de>
2024-10-04 15:18:15 +0200

    Changed config for bootstrap

--- app/client/vite.config.ts
+++ app/client/vite.config.ts
@@ -5,17 +5,28 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-  ],
+  plugins: [vue()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
-      "@common": fileURLToPath(new URL("../common/src", import.meta.url))
+      '@common': fileURLToPath(new URL('../common/src', import.meta.url))
     }
   },
   build: {
-    outDir: "../dist/client",
-    emptyOutDir: false
+    outDir: '../dist/client',
+    emptyOutDir: false,
+    sourcemap: true,
+    minify: true,
+    cssMinify: true,
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        // WORKARROUND this is needed because of a new version of scss
+        // For more information see: https://github.com/twbs/bootstrap/issues/40849
+        quietDeps: true,
+        silenceDeprecations: ['legacy-js-api']
+      }
+    }
   }
 })
