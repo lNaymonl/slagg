dc6e7d1facdf278e4604afc07059fb971cd6cb89
Elmo Heine <elmo.heine@web.de>
2024-10-16 21:38:10 +0200

    Added a custom plugin to prevent old drizzle files to stack up in the build folder

--- app/esbuild.mjs
+++ app/esbuild.mjs
@@ -1,5 +1,8 @@
 import { build } from "esbuild";
 
+import * as fs from "fs";
+import path from "path";
+
 // Has to be imported weirdly because it is a commonjs import insde an esm module
 import yamlPluginImp from "esbuild-plugin-yaml";
 const { yamlPlugin } = yamlPluginImp;
@@ -7,10 +10,22 @@ import copy from "esbuild-plugin-copy";
 
 const production = process.argv[2] == "--prod";
 
+const BUILD_FOLDER = "./dist";
+
+// Custom plugin to remove the `drizzle/` directory from the build folder
+let removeDrizzlePlugin = {
+    name: "Remove Drizzle Folder in the build Folder",
+    setup(build) {
+        build.onStart(() => {
+            fs.rmSync(path.join(BUILD_FOLDER, "/drizzle"), { recursive: true, force: true })
+        });
+    },
+};
+
 console.info("Building Server with env:", production ? "production" : "development");
 await build({
     entryPoints: ["./server/src/index.ts"],
-    outfile: "./dist/index.js",
+    outfile: path.join(BUILD_FOLDER, "/index.js"),
     bundle: true,
     platform: "node",
     external: [
@@ -26,6 +41,7 @@ await build({
     logLevel: "info",
     minify: production, // Minifys the output when in production mode
     plugins: [
+        removeDrizzlePlugin,
         yamlPlugin(), // Plugin to parse .yaml files
         copy({ // Copies every nessecary file to the build folder
             assets: [
