d0318c1d46ba8e68f8057a8a610aa578b33190b4
FRobben99 <113891357+FRobben99@users.noreply.github.com>
2024-10-22 09:42:17 +0200

    Fix alignment

--- app/server/src/routes/user.router.ts
+++ app/server/src/routes/user.router.ts
@@ -1,15 +1,15 @@
-import { Request, Response } from 'express';
-import { Router } from "express";
-import  jwt from 'jsonwebtoken';
-import { getPrivateKey } from '../util/secret-provider'
+import {Request, Response} from 'express';
+import {Router} from "express";
+import jwt from 'jsonwebtoken';
+import {getPrivateKey} from '../util/secret-provider'
 
 
 const router = Router();
 
-router.post("/login",( req: Request, res: Response) => {
-    const { username, password } = req.body;
-        if( username === "Karl" && password === "test"){
-            const payload = { username };
+router.post("/login", (req: Request, res: Response) => {
+        const {username, password} = req.body;
+        if (username === "Karl" && password === "test") {
+            const payload = {username};
 
             const token = jwt.sign(payload, getPrivateKey(), {
                 algorithm: 'RS256',
